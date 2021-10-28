f2e58912c6e8641eddbef208f37027be24b857dd
aimen08 <aimen_hamza@hotmail.com>
2021-10-28 16:27:31 +0100

    add redirect

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-postcss",
+    `gatsby-plugin-netlify`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-google-fonts`,
